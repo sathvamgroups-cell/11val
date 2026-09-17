d89ef439693f703b3a4f71590008d467b46345a6
sathvamgroups-cell <sathvamgroups@gmail.com>
2026-09-17 15:16:48 +0530

    Create user.go

---
+++ user.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	ID    uint   `json:"id" gorm:"primaryKey"`
+	Name  string `json:"name" validate:"required,min=3,max=50"`
+	Email string `json:"email" validate:"required,email"`
+	Role  string `json:"role" validate:"required,oneof=admin user"`
+	Age   int    `json:"age" validate:"gte=18,lte=100"`
+}
+
+var DB *gorm.DB
